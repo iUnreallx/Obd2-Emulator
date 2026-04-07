90ab4efd53a98263ae3e508b295bbf0b08ea851f
Unreallx <artemx2224@yandex@gmail.com>
2026-04-08 00:06:13 +0300

    feat(serial): improve serial port handling and data processing

--- Src/Header/SerialPortWorker.h
+++ Src/Header/SerialPortWorker.h
@@ -14,19 +14,27 @@ public:
 public slots:
     void openPort(const QString &portName);
     void closePort();
+
     void changeSpeedToOBD(int speed);
+    void changeRpmToOBD(int rpm);
+    void changeTempToOBD(int temp);
+    void changeVoltageToOBD(double voltage);
 
 signals:
     void portOpened(bool success);
-    void dataReceived(const QByteArray &data);
     void errorOccurred(const QString &error);
+    void dataReceived(const QByteArray &data);
 
 private slots:
     void handleReadyRead();
 
 private:
     QSerialPort *serial;
+
     int m_speed = 0;
+    int m_rpm = 0;
+    int m_temp = 90;
+    double m_voltage = 14.2;
 };
 
 #endif // SERIALPORTWORKER_H
--- Src/Source/SerialPortConnector.cpp
+++ Src/Source/SerialPortConnector.cpp
@@ -3,7 +3,6 @@
 #include <QDebug>
 #include <QThread>
 
-
 SerialPortConnector::SerialPortConnector(QObject *parent)
     : QObject(parent), workerThread(new QThread(this)), worker(new SerialPortWorker())
 {
@@ -34,6 +33,7 @@ void SerialPortConnector::openPort(const QString &portName)
         } else {
             qDebug() << "Failed to open port.";
         }
+        emit connectionResult(success);
         disconnect(worker, &SerialPortWorker::portOpened, this, nullptr);
     });
 }
@@ -43,7 +43,24 @@ void SerialPortConnector::closePort()
     QMetaObject::invokeMethod(worker, "closePort", Qt::QueuedConnection);
 }
 
+// --- БЕЗОПАСНАЯ ПЕРЕДАЧА ДАННЫХ В ПОТОК ---
+
 void SerialPortConnector::changeSpeedToOBD(int speed)
 {
     QMetaObject::invokeMethod(worker, "changeSpeedToOBD", Qt::QueuedConnection, Q_ARG(int, speed));
+}
+
+void SerialPortConnector::changeRpmToOBD(int rpm)
+{
+    QMetaObject::invokeMethod(worker, "changeRpmToOBD", Qt::QueuedConnection, Q_ARG(int, rpm));
+}
+
+void SerialPortConnector::changeTempToOBD(int temp)
+{
+    QMetaObject::invokeMethod(worker, "changeTempToOBD", Qt::QueuedConnection, Q_ARG(int, temp));
+}
+
+void SerialPortConnector::changeVoltageToOBD(double voltage)
+{
+    QMetaObject::invokeMethod(worker, "changeVoltageToOBD", Qt::QueuedConnection, Q_ARG(double, voltage));
 }
